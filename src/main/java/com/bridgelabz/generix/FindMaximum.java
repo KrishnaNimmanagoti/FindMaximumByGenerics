2a1c78cfb68d0fdc8ff7fc637f2f400aec11bb60
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-03-10 00:19:29 +0530

    Refactored to Compare Floats

--- src/main/java/com/bridgelabz/generix/FindMaximum.java
+++ src/main/java/com/bridgelabz/generix/FindMaximum.java
@@ -2,8 +2,19 @@ package com.bridgelabz.generix;
 
 public class FindMaximum {
 
-    public Integer findMax(Integer num1, Integer num2, Integer num3) {
+    public Integer findMaxInteger(Integer num1, Integer num2, Integer num3) {
         Integer max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        return max;
+    }
+
+    public Float findMaxFloat(Float num1, Float num2, Float num3) {
+        Float max = num1;
         if (num2.compareTo(max) > 0) {
             max = num2;
         }
