88cc16171a6b01dd4b2f501a5309ece536954e45
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-03-10 13:10:46 +0530

    Refactored to create generic class to take 3 variables in

--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -1,39 +1,27 @@
 import com.bridgelabz.generix.FindMaximum;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FindMaximumTest {
-    FindMaximum findMaximum ;
 
     @Test
     public void compareIntegers(){
-        findMaximum = new FindMaximum(20, 10, 2);
-        findMaximum = new FindMaximum(10, 20, 2);
-        findMaximum = new FindMaximum(10, 2, 20);
-        Assertions.assertEquals(20, findMaximum.maximum());
-        Assertions.assertEquals(20, findMaximum.maximum());
-        Assertions.assertEquals(20, findMaximum.maximum());
+        Assertions.assertEquals(20, new FindMaximum<>(20, 10, 2).maximum());
+        Assertions.assertEquals(20, new FindMaximum<>(10, 20, 2).maximum());
+        Assertions.assertEquals(20, new FindMaximum<>(10, 2, 20).maximum());
     }
 
     @Test
     public void compareFloats(){
-
-        findMaximum = new FindMaximum(20.2f, 10.3f, 2.4f);
-        findMaximum = new FindMaximum(10.3f, 20.2f, 2.4f);
-        findMaximum = new FindMaximum(10.3f, 2.4f, 20.2f);
-        Assertions.assertEquals(20.2f, findMaximum.maximum());
-        Assertions.assertEquals(20.2f, findMaximum.maximum());
-        Assertions.assertEquals(20.2f, findMaximum.maximum());
+        Assertions.assertEquals(20.2f, new FindMaximum<>(20.2f, 10.3f, 2.4f).maximum());
+        Assertions.assertEquals(20.2f, new FindMaximum<>(10.3f, 20.2f, 2.4f).maximum());
+        Assertions.assertEquals(20.2f, new FindMaximum<>(10.3f, 2.4f, 20.2f).maximum());
     }
 
     @Test
     public void compareSting(){
-        findMaximum = new FindMaximum("Mango", "Apple", "Grapes");
-        findMaximum = new FindMaximum("Apple", "Mango", "Grapes");
-        findMaximum = new FindMaximum("Apple", "Grapes", "Mango");
-        Assertions.assertEquals("Mango", findMaximum.maximum());
-        Assertions.assertEquals("Mango", findMaximum.maximum());
-        Assertions.assertEquals("Mango", findMaximum.maximum());
+        Assertions.assertEquals("Mango", new FindMaximum<>("Mango", "Apple", "Grapes").maximum());
+        Assertions.assertEquals("Mango", new FindMaximum<>("Apple", "Mango", "Grapes").maximum());
+        Assertions.assertEquals("Mango", new FindMaximum<>("Apple", "Grapes", "Mango").maximum());
     }
 }
